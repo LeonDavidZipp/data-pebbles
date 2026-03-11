555512450d7c548187412f6d975bbd4dfb0277fd
Leon David Zipp <leondavidzipp@gmx.de>
2026-03-12 00:15:05 +0100

    feat: added metadata tables for silver and gold data sources

--- init.sql
+++ init.sql
@@ -9,18 +9,18 @@ CREATE SCHEMA IF NOT EXISTS platinum;
 CREATE TYPE file_status AS ENUM ('active', 'archived', 'deleted');
 
 CREATE TABLE IF NOT EXISTS bronze.source_metadata (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     s3_bucket TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX IF NOT EXISTS idx_source_metadata_s3_bucket ON internal.source_metadata (s3_bucket);
+CREATE INDEX IF NOT EXISTS idx_source_metadata_s3_bucket ON bronze.source_metadata (s3_bucket);
 
 CREATE TABLE IF NOT EXISTS bronze.source_versions (
-    id SERIAL PRIMARY KEY,
-    source_id INTEGER NOT NULL REFERENCES internal.source_metadata (id) ON DELETE CASCADE,
+    id BIGSERIAL PRIMARY KEY,
+    source_id BIGINT NOT NULL REFERENCES bronze.source_metadata (id) ON DELETE CASCADE,
     status file_status NOT NULL DEFAULT 'active',
     s3_key TEXT UNIQUE NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -31,3 +31,23 @@ CREATE TABLE IF NOT EXISTS bronze.source_versions (
 CREATE INDEX IF NOT EXISTS idx_source_versions_source_id ON internal.source_versions (source_id);
 
 CREATE INDEX IF NOT EXISTS idx_source_versions_status ON internal.source_versions (status);
+
+CREATE TABLE IF NOT EXISTS silver.source_metadata (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    from_source_ids BIGINT[] NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_silver_source_metadata_name ON silver.source_metadata (name);
+
+CREATE TABLE IF NOT EXISTS gold.source_metadata (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    from_source_ids BIGINT[] NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_gold_source_metadata_name ON gold.source_metadata (name);
