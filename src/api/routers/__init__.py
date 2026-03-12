c89d2622bd71e4b8b3858958e7813be9bb7d3b4d
Leon David Zipp <leondavidzipp@gmx.de>
2026-03-12 16:55:31 +0100

    feat: reouer integration into app

---
+++ src/api/routers/__init__.py
@@ -0,0 +1,5 @@
+from .bronze import bronze_router
+from .gold import gold_router
+from .silver import silver_router
+
+__all__ = ["bronze_router", "silver_router", "gold_router"]
--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
-def main():
-	print("Hello from data-pebbles!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from .api import bronze_router, gold_router, silver_router
 
-if __name__ == "__main__":
-	main()
+app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+
+app.include_router(
+	bronze_router,
+	prefix="/bronze",
+	tags=["API Endpoints for interacting with the Bronze layer"],
+)
+app.include_router(
+	silver_router,
+	prefix="/silver",
+	tags=["API Endpoints for interacting with the Silver layer"],
+)
+app.include_router(
+	gold_router,
+	prefix="/gold",
+	tags=["API Endpoints for interacting with the Gold layer"],
+)
