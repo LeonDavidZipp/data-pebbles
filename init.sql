3955cf98a9ecafea2e2f6a90d56b88204b292bea
Leon David Zipp <leondavidzipp@gmx.de>
2026-03-12 11:30:58 +0100

    refactor: reordered init.sql into one dedicated section per layer
    fix: rm s3_bucket TEXT NOT NULL column, since bucket will always be bronze (file structure handled by postgres)

--- init.sql
+++ init.sql
@@ -1,40 +1,40 @@
+-- bronze layer
+
 CREATE SCHEMA IF NOT EXISTS bronze;
 
-CREATE SCHEMA IF NOT EXISTS silver;
-
-CREATE SCHEMA IF NOT EXISTS gold;
-
-CREATE SCHEMA IF NOT EXISTS platinum;
-
-CREATE TYPE file_status AS ENUM ('active', 'archived', 'deleted');
+CREATE TYPE bronze.file_status AS ENUM ('active', 'archived', 'deleted');
 
 CREATE TABLE IF NOT EXISTS bronze.source_metadata (
     id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    s3_bucket TEXT NOT NULL,
+    name TEXTNOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX IF NOT EXISTS idx_source_metadata_s3_bucket ON bronze.source_metadata (s3_bucket);
+CREATE INDEX IF NOT EXISTS idx_source_metadata_name ON bronze.source_metadata (name);
 
 CREATE TABLE IF NOT EXISTS bronze.source_versions (
     id BIGSERIAL PRIMARY KEY,
     source_id BIGINT NOT NULL REFERENCES bronze.source_metadata (id) ON DELETE CASCADE,
+    version BIGINT NOT NULL,
     status file_status NOT NULL DEFAULT 'active',
     s3_key TEXT UNIQUE NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    UNIQUE (source_id, version_number)
+    UNIQUE (source_id, version)
 );
 
 CREATE INDEX IF NOT EXISTS idx_source_versions_source_id ON internal.source_versions (source_id);
 
 CREATE INDEX IF NOT EXISTS idx_source_versions_status ON internal.source_versions (status);
 
+-- silver layer
+
+CREATE SCHEMA IF NOT EXISTS silver;
+
 CREATE TABLE IF NOT EXISTS silver.source_metadata (
     id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXTNOT NULL,
     from_source_ids BIGINT[] NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -42,9 +42,13 @@ CREATE TABLE IF NOT EXISTS silver.source_metadata (
 
 CREATE INDEX IF NOT EXISTS idx_silver_source_metadata_name ON silver.source_metadata (name);
 
+-- gold layer
+
+CREATE SCHEMA IF NOT EXISTS gold;
+
 CREATE TABLE IF NOT EXISTS gold.source_metadata (
     id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXTNOT NULL,
     from_source_ids BIGINT[] NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
