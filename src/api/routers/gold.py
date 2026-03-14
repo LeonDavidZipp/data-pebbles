eb60ca512b878282767845c6e4d9936d9dd05146
Leon David Zipp <leondavidzipp@gmx.de>
2026-03-14 12:22:05 +0100

    fix: params passde to methods in gild and silver endpoints

--- src/api/routers/gold.py
+++ src/api/routers/gold.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 import polars as pl
-from fastapi import APIRouter, Depends, Query, UploadFile
+from fastapi import APIRouter, Depends, Path, Query, UploadFile
 from fastapi.responses import StreamingResponse
 
 from ..dependencies import DeltaLoader, gold_dep, validate_file
@@ -16,20 +16,20 @@ async def upload(
 ):
 	content = await file.read()
 	lf = pl.scan_parquet(content)
-	gold.upload_lazy(table=file.filename, lf=lf, mode="append")  # type: ignore
+	gold.upload(table=file.filename, lf=lf, mode="append")  # type: ignore
 
 
-@gold_router.get("/download/{file_id}")
+@gold_router.get("/download/{source_id}")
 def download(
 	gold: Annotated[DeltaLoader, Depends(gold_dep)],
-	file_id: int,
+	source_id: Annotated[int, Path()],
 	version: Annotated[int | None, Query()] = None,
 ) -> StreamingResponse:
-	df = gold.get(table=str(file_id), version=version)
+	df = gold.get(table=str(source_id), version=version).collect()
 	buf = df.write_ipc_stream(None)
 	buf.seek(0)
 	return StreamingResponse(
 		buf,
 		media_type="application/octet-stream",
-		headers={"Content-Disposition": f"attachment; filename={file_id}.parquet"},
+		headers={"Content-Disposition": f"attachment; filename={source_id}.parquet"},
 	)
--- src/api/routers/silver.py
+++ src/api/routers/silver.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 import polars as pl
-from fastapi import APIRouter, Depends, Query, UploadFile
+from fastapi import APIRouter, Depends, Path, Query, UploadFile
 from fastapi.responses import StreamingResponse
 
 from ..dependencies import DeltaLoader, silver_dep, validate_file
@@ -9,14 +9,15 @@ from ..dependencies import DeltaLoader, silver_dep, validate_file
 silver_router = APIRouter()
 
 
-@silver_router.post("/upload")
+@silver_router.post("/upload/{source_id}")
 async def upload(
+	source_id: Annotated[int, Path()],
 	file: Annotated[UploadFile, Depends(validate_file)],
 	silver: Annotated[DeltaLoader, Depends(silver_dep)],
 ):
 	content = await file.read()
-	df = pl.read_parquet(content)
-	silver.upload(table=file.filename, df=df, mode="append")  # type: ignore
+	lf = pl.scan_parquet(content)
+	silver.upload(table=file.filename, lf=lf, mode="append")  # type: ignore
 
 
 @silver_router.get("/download/{file_id}")
@@ -25,7 +26,7 @@ def download(
 	silver: Annotated[DeltaLoader, Depends(silver_dep)],
 	version: Annotated[int | None, Query()] = None,
 ) -> StreamingResponse:
-	df = silver.get(table=str(file_id), version=version)
+	df = silver.get(table=str(file_id), version=version).collect()
 	buf = df.write_ipc_stream(None)
 	buf.seek(0)
 	return StreamingResponse(
