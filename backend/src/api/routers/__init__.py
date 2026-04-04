963796a2a9a30b2e76b40e1f24c043c246123ad0
Leon David Zipp <leondavidzipp@gmx.de>
2026-04-04 14:22:42 +0200

    feat: added actual bronze router

--- backend/src/api/routers/__init__.py
+++ backend/src/api/routers/__init__.py
@@ -1,6 +1,13 @@
+from .bronze import bronze_router
 from .gold import gold_router
 from .projects import projects_router
 from .raw import raw_router
 from .silver import silver_router
 
-__all__ = ["raw_router", "silver_router", "gold_router", "projects_router"]
+__all__ = [
+	"bronze_router",
+	"gold_router",
+	"projects_router",
+	"raw_router",
+	"silver_router",
+]
